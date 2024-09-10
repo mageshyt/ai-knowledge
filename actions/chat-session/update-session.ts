99416b06df7193b07a4b99405d8b0c59f62bce57
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-10 23:17:49 +0530

    :sparkles:  Update sidebar item component to allow editing and deleting chat sessions

---
+++ actions/chat-session/update-session.ts
@@ -0,0 +1,52 @@
+
+"use server";
+import { db } from "@/lib";
+import { auth } from "@clerk/nextjs/server";
+
+type ChatSessionProps = {
+  sessionId: string;
+  value: Record<string, unknown>;
+}
+export const updateSession = async ({ sessionId, value }: ChatSessionProps) => {
+  try {
+    const { userId } = auth()
+
+    if (!userId) {
+      return {
+        error: "You are not authenticated"
+      }
+    }
+
+    // check if the use is the owner of the session
+    const session = await db.chatSession.findFirst({
+      where: {
+        userId,
+        id: sessionId
+      }
+    })
+
+    if (!session) {
+      return {
+        error: "You are not the owner of the session"
+      }
+    }
+
+    const updatedSession = await db.chatSession.update({
+      where: {
+        id: sessionId,
+        userId: userId
+      },
+      data: {
+        updatedAt: new Date(),
+        ...value
+      }
+    })
+
+    console.log('[createSession] updatedSession', updatedSession)
+    return updatedSession;
+  }
+  catch (error) {
+    console.error('[createSession] error', error)
+    return null
+  }
+}
--- app/(main)/(routes)/chats/components/sidebar-item.tsx
+++ app/(main)/(routes)/chats/components/sidebar-item.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { toast } from 'sonner';
 
-import { Ellipsis, Pencil, Share, Trash } from 'lucide-react';
+import { Archive, Ellipsis, Pencil, Share, Trash } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,6 +11,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { updateSession } from '@/actions/chat-session/update-session';
 
 interface SidebarItemProps {
   id: string;
@@ -18,34 +21,88 @@ interface SidebarItemProps {
 }
 
 export const SidebarItem = ({ id, name }: SidebarItemProps) => {
+  //------------------State-------------------
+  const [edit, setEdit] = useState<boolean>(false)
+  const [editName, setEditName] = useState<string>(name)
+  const [updating, setUpdating] = useState<boolean>(false)
 
+
+
+  //------------------Handlers-------------------
 
   // TODO: Add the ability to edit and delete chat session
-  const handleEdit = () => {
-    toast.error('Edit feature is not available yet')
+  const handleEdit = async () => {
+
+    try {
+      setUpdating(true)
+
+      const response = await updateSession({ sessionId: id, value: { name: editName } })
+
+      if (response && "error" in response) {
+      // Revert the name back to the original name
+        toast.error(response.error)
+        setEditName(name)
+        return;
+      }
+
+      if (response) {
+        toast.success('Session updated successfully')
+        setEdit(false)
+
+      }
+    }
+    catch (error) {
+      console.error(error)
+      // Revert the name back to the original name
+      setEditName(name)
+      toast.error('Failed to update sessionId')
+    }
+    finally {
+      setUpdating(false)
+    }
+  }
+
+
+  const handleShare = () => {
+    toast.error('Share feature is not available yet')
   }
 
   const handleDelete = () => {
     toast.error('Delete feature is not available yet')
   }
 
-  const handleShare = () => {
-    toast.error('Share feature is not available yet')
-  }
-
-
   return (
     <div
       className='relative flex group items-center gap-2 px-4 py-3  rounded-md text-sm hover:dark:bg-neutral-800 hover:bg-slate-100'
     >
 
-      <Link
-        href={`/chats/${id}`}
-        className='w-full h-full rounded-md '
-      >
-        {name}
+      {
+        edit ? (
+          <Input
+            onChange={(e) => setEditName(e.target.value)}
+            disabled={updating}
+            onKeyPress={(e) => {
+              console.log(e.key) // Handle Enter and Escape key
+              if (e.key === 'Enter') 
+                handleEdit()
+              }
+            }
 
-      </Link>
+            type='text'
+            className='w-full h-full rounded-md'
+            value={editName}
+          />
+        )
+          :
+
+          <Link
+            href={`/chats/${id}`}
+            className='w-full h-full rounded-md '
+          >
+            {editName}
+
+          </Link>
+      }
       {/* Button to edit  */}
       <DropdownMenu
       >
@@ -56,7 +113,7 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
             className='group-hover:opacity-100 opacity-0 focus:opacity-100 outline-none focus:outline-none'
           >
 
-          <Ellipsis className="size-4 ml-2 " />
+            <Ellipsis className="size-4 ml-2 " />
           </button>
         </DropdownMenuTrigger>
         {/* ----------------Button to Edit and Share-------------- */}
@@ -64,7 +121,7 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
           className='w-40'
         >
           <DropdownMenuItem
-            onClick={handleEdit}
+            onClick={() => setEdit(!edit)}
           >
             <Pencil className="size-4 mr-2" />
             Edit
@@ -80,15 +137,23 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
 
           <DropdownMenuSeparator />
 
-        {/* ----------------Button to delete-------------- */}
+          {/* ----------------Button to delete-------------- */}
+
+          <DropdownMenuItem
+            onClick={handleDelete}
+          >
+            <Archive className="size-4 mr-2" />
+            Archive
+          </DropdownMenuItem>
 
           <DropdownMenuItem
             className='text-red-500'
             onClick={handleDelete}
           >
-            <Trash className="size-4 mr-2" /> 
+            <Trash className="size-4 mr-2" />
             Delete
           </DropdownMenuItem>
+
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
