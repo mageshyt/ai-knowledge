028b56da610e21f33e4bd9d819bcc4ef62167290
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-11 22:17:48 +0530

    Update sidebar item component to include archive and delete functionality

--- app/(main)/(routes)/chats/components/sidebar-item.tsx
+++ app/(main)/(routes)/chats/components/sidebar-item.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { toast } from 'sonner';
 import { useKey } from "react-use";
 import { usePathname, useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 import { Archive, Ellipsis, Pencil, Share, Trash } from 'lucide-react';
 import {
@@ -17,7 +18,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { updateSession } from '@/actions/chat-session/update-session';
 import { useModal } from '@/hooks/use-modal';
-import { cn } from '@/lib';
+import { deleteSession } from '@/actions/chat-session/delete-session';
 
 interface SidebarItemProps {
   id: string;
@@ -77,13 +78,34 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
   }
 
 
-  const handleShare = () => {
+  const handleShare = async () => {
     toast.error('Share feature is not available yet')
   }
 
 
-  const handleDelete = () => {
-    toast.error('Delete feature is not available yet')
+  const handleDelete = async () => {
+    try {
+      setUpdating(true)
+      const response = await deleteSession({ sessionId: id })
+
+      if (response && "error" in response) {
+        toast.error(response.error)
+        return;
+      }
+
+      toast.success('Session deleted successfully');
+
+      // Redirect to the chats page
+      router.push('/chats')
+    }
+    catch (error) {
+      console.error(error)
+      toast.error('Failed to delete sessionId')
+    }
+    finally {
+      setUpdating(false)
+
+    }
   }
 
   const handleArchive = async () => {
@@ -113,9 +135,7 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
   return (
     <div
       className={cn('relative flex group items-center gap-2 px-4 py-3  rounded-md text-sm hover:dark:bg-neutral-800 hover:bg-slate-100',
-
         isActivity ? 'bg-slate-100 dark:bg-neutral-800' : 'dark:hover:bg-neutral-800'
-
       )}
     >
 
@@ -153,7 +173,10 @@ export const SidebarItem = ({ id, name }: SidebarItemProps) => {
           asChild
         >
           <button
-            className='group-hover:opacity-100 opacity-0 focus:opacity-100 outline-none focus:outline-none'
+            className={cn('group-hover:opacity-100 opacity-0 focus:opacity-100 outline-none focus:outline-none',
+              edit || isActivity ? 'opacity-100' : 'opacity-0',
+
+            )}
           >
 
             <Ellipsis className="size-4 ml-2 " />
