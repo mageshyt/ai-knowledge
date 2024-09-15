73147665bd2718049e701d46729647a54387c241
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-15 13:36:37 +0530

    Refactor navbar component to include a flexible empty space

--- app/(main)/(routes)/chats/components/navbar.tsx
+++ app/(main)/(routes)/chats/components/navbar.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import MobileSidebar from "./mobile-sidebar";
 import { SocketIndicatorBadge } from "@/components/global/socket-indicator-badge";
@@ -7,6 +6,7 @@ const Navbar = () => {
   return (
     <div className="flex bg-background justify-between items-center p-4 border-b h-full ">
       <MobileSidebar />
+      <div className="flex-grow"></div>
       <SocketIndicatorBadge />
     </div>
   );
