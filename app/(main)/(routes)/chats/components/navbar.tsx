328913efaa4c12a59d7b1528f8bb91eee3e6dd14
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-15 21:09:15 +0530

    :recycle: Refactor navbar component to include NavbarRoutes

--- app/(main)/(routes)/chats/components/navbar.tsx
+++ app/(main)/(routes)/chats/components/navbar.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import MobileSidebar from "./mobile-sidebar";
 import { SocketIndicatorBadge } from "@/components/global/socket-indicator-badge";
+import MobileSidebar from "./mobile-sidebar";
+import { NavbarRoutes } from "./navbar-routes";
 
 const Navbar = () => {
   return (
     <div className="flex bg-background justify-between items-center p-4 border-b h-full ">
       <MobileSidebar />
       <div className="flex-grow"></div>
-      <SocketIndicatorBadge />
+      <NavbarRoutes />
     </div>
   );
 };
