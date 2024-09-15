5d093aded7957cf2d51be14c94ae8825580233b1
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-15 12:57:57 +0530

    :alembic: socket setup done

--- app/(main)/(routes)/chats/components/navbar.tsx
+++ app/(main)/(routes)/chats/components/navbar.tsx
@@ -1,11 +1,13 @@
 
 import React from "react";
 import MobileSidebar from "./mobile-sidebar";
+import { SocketIndicatorBadge } from "@/components/global/socket-indicator-badge";
 
 const Navbar = () => {
   return (
-    <div className="flex bg-background items-center p-4 border-b h-full ">
+    <div className="flex bg-background justify-between items-center p-4 border-b h-full ">
       <MobileSidebar />
+      <SocketIndicatorBadge />
     </div>
   );
 };
