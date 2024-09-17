fc5f6420fa4c5bebdb3c0c3f67ee54e0c5e2d25f
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-17 13:41:07 +0530

    :fire:  navbar routes component and remove share modal

--- app/(main)/(routes)/chats/components/navbar-routes.tsx
+++ app/(main)/(routes)/chats/components/navbar-routes.tsx
@@ -1,16 +1,11 @@
-import { Button } from '@/components/ui/button'
 import { SocketIndicatorBadge } from '@/components/global/socket-indicator-badge'
-import { ShareModal } from '@/components/modals/share-modal'
 import { SettingsSidebar } from './settings-sidebar'
 
 export const NavbarRoutes = () => {
   return (
     <div className='flex items-center gap-2'>
-
       <SocketIndicatorBadge />
-      <ShareModal />
       <SettingsSidebar/>
-
     </div>
   )
 }
