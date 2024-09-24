97403b41d9a40d7a639ed2b4cdffe3755fb87539
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-24 17:48:32 +0530

    Refactor currentProfile function to currentProfilePage for clarity

--- lib/current-profile-pages.ts
+++ lib/current-profile-pages.ts
@@ -2,7 +2,7 @@ import {  getAuth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { NextApiRequest } from "next";
 
-export const currentProfile = (req:NextApiRequest) => {
+export const currentProfilePage = (req:NextApiRequest) => {
   const { userId } = getAuth(req);
 
   if (!userId) {
