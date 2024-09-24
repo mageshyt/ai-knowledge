6b67a9f87a962f7cc21d63ac03ffa04f0d8bd28f
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-24 19:05:47 +0530

    :zap: routes to get all message of the session

--- app/(main)/(routes)/chats/[...chatId]/components/messages.tsx
+++ app/(main)/(routes)/chats/[...chatId]/components/messages.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Message as TMessage } from 'ai/react';
 import ListView from '@/components/global/list-view';
 import { MessageContent } from './message-content';
 import { MessageSquare } from 'lucide-react';
+import tw from 'tailwind-styled-components';
+import { MessageWithUserProfile } from '@/typings/typing';
 
 interface MessagesProps {
-  messages: TMessage[];
+  messages: MessageWithUserProfile[];
 }
 export const Messages = ({ messages }: MessagesProps) => {
+  console.log(messages)
   return (
-    <div
-      className=' flex-1 flex flex-col py-4 gap-y-2 overflow-y-scroll  scrollbar-thin scrollbar-thumb-zinc-800 '
-    >
+    <Wrapper >
       {
         messages.length > 0 ? <ListView
           items={messages}
-          render={(message: TMessage) => <MessageContent
+          render={(message: MessageWithUserProfile) => <MessageContent
             content={message.content}
             isUserMessage={message.role === 'user'}
           />}
@@ -23,11 +23,17 @@ export const Messages = ({ messages }: MessagesProps) => {
           <div className="flex-1 flex flex-col items-center justify-center gap-2">
             <MessageSquare className="size-10 text-blue-500" />
             <h3 className="font-semibold text-xl dark:text-white">Ready to roll!</h3>
-            <p className="text-zinc-500 text-sm">Go ahead, ask your first question and explore.</p>
+            <p className="text-zinc-500 text-sm">
+              Go ahead, ask your first question and explore.
+            </p>
           </div>
       }
 
-    </div>
+    </Wrapper>
   )
 }
-
+const Wrapper = tw.div`
+flex-1 flex flex-col
+py-4 gap-y-2 overflow-y-scroll
+scrollbar-thin scrollbar-thumb-zinc-800 
+    `
---
+++ app/api/messages/route.ts
@@ -0,0 +1,88 @@
+import { db } from "@/lib";
+import { currentProfile } from "@/lib/current-profile";
+import { Message } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+
+const MESSAGE_BATCH = 10
+
+export async function GET(
+  req: Request
+) {
+  try {
+    const user = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const cursor = searchParams.get('cursor');
+    const sessionId = searchParams.get('sessionId');
+
+    // check if user exist or not
+    if (!user) {
+      return new NextResponse("Unauthroized", { status: 400 });
+    }
+
+    if (!sessionId) {
+
+      return new NextResponse("Missing SessionID", { status: 400 });
+    }
+
+    let messages: Message[] = []
+
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        where: {
+          chatId: sessionId,
+        },
+        take: MESSAGE_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        include: {
+          user: true
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      },
+
+      )
+    }
+    else {
+      messages = await db.message.findMany({
+        where: {
+          chatId: sessionId,
+        },
+        take: MESSAGE_BATCH,
+        include: {
+          user: true
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      },
+      )
+    }
+
+    let nextCursor = null;
+
+    // if we have more messages to fetch
+    if (messages.length === MESSAGE_BATCH) {
+      nextCursor = messages[messages.length - 1].id
+    }
+    // once we have all the messages we dont have any next cursor so it will be null
+
+
+    return NextResponse.json({
+      messages: messages,
+      nextCursor
+    });
+  }
+
+  catch (err) {
+    console.log('[MESSEGES GET]', err);
+    return new NextResponse("Internal Error", { status: 500 })
+
+  }
+}
