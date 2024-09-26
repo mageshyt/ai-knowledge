97dfb2c2058f1566860ff350ea06907dd22fe009
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-26 21:03:23 +0530

    Refactor chat scroll behavior for better user experience

---
+++ hooks/use-chat-scroll.ts
@@ -0,0 +1,67 @@
+"use client";
+import { useEffect, useState } from "react";
+
+type ChatScrollProps = {
+  chatRef: React.RefObject<HTMLDivElement>;
+  bottomRef: React.RefObject<HTMLDivElement>;
+  shouldLoadMore: boolean;
+  loadMore: () => void;
+  count: number;
+};
+
+export const useChatScroll = ({
+  chatRef,
+  bottomRef,
+  shouldLoadMore,
+  loadMore,
+  count,
+}: ChatScrollProps) => {
+  console.log("useChatScroll",count);
+  const [hasInitialized, setHasInitialized] = useState(false);
+
+  useEffect(() => {
+    const topDiv = chatRef?.current;
+
+    const handleScroll = () => {
+      const scrollTop = topDiv!.scrollTop;
+
+      if (scrollTop === 0 && shouldLoadMore) {
+        loadMore();
+      }
+    };
+    topDiv?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      topDiv?.removeEventListener("scroll", handleScroll);
+    };
+  }, [shouldLoadMore, loadMore, chatRef]);
+
+  useEffect(() => {
+    const bottomDiv = bottomRef?.current;
+
+    const topDiv = chatRef?.current;
+    console.log("bottomDiv", bottomDiv);
+    console.log("topDiv", topDiv);
+
+    const shouldAutoScroll = () => {
+      if (!hasInitialized && bottomDiv) {
+        setHasInitialized(true);
+        return true;
+      }
+
+      if (!topDiv) return false;
+
+      const distanceFromBottom = topDiv.scrollHeight - topDiv.scrollTop;
+      console.log(distanceFromBottom);
+
+      return distanceFromBottom < 800;
+    };
+
+    if (shouldAutoScroll()) {
+      setTimeout(() => {
+        console.log("scrolling");
+        bottomDiv!.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  }, [chatRef, bottomRef, count, hasInitialized]);
+};
