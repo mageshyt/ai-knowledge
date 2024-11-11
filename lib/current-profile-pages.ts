c71c7ea7d575392b37f27282518489a7c12b55b8
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-11-11 23:04:46 +0530

    added caching to the fetceh user

--- lib/current-profile-pages.ts
+++ lib/current-profile-pages.ts
@@ -1,18 +1,31 @@
-import {  getAuth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { NextApiRequest } from "next";
+import { redis } from "./redis";
+import { User } from "@prisma/client";
 
-export const currentProfilePage = (req:NextApiRequest) => {
+export const currentProfilePage = async (req: NextApiRequest) => {
   const { userId } = getAuth(req);
 
   if (!userId) {
     return null;
   }
 
-  const user = db.user.findFirst({
+  const cacheUser = await redis.get(userId);
+
+  if (cacheUser) {
+    return cacheUser as User;
+  }
+
+
+  const user = await db.user.findFirst({
     where: {
       id: userId,
     },
+  });
+
+  await redis.set(userId, JSON.stringify(user), {
+    ex: 60 * 15
   });
 
   return user;
--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -1,18 +1,35 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
 
-export const currentProfile = () => {
+import { redis } from "./redis";
+import { User } from "@prisma/client";
+
+export const currentProfile = async () => {
   const { userId } = auth();
 
   if (!userId) {
     return null;
   }
+  // check if the user is in the cache
+  const cacheUser = await redis.get(userId);
 
-  const user = db.user.findFirst({
+
+  if (cacheUser) {
+    return cacheUser as User;
+  }
+
+  // if the user is not in the cache, fetch from the database
+
+
+  const user = await db.user.findFirst({
     where: {
       id: userId,
     },
   });
 
+  // store the user in the cache
+  await redis.set(userId, JSON.stringify(user), {
+    ex: 60 * 15
+  });
   return user;
 };
