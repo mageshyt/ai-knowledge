93520362226ea7535544eb2bc66cf6e511f1ee0f
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-11-17 23:06:34 +0530

    🐛 major bug fixed in manage session cause server error while logout

--- app/(main)/(routes)/chats/components/manage-session.tsx
+++ app/(main)/(routes)/chats/components/manage-session.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { redirect, usePathname } from "next/navigation";
-import { useAuth } from "@clerk/nextjs";
+import {  usePathname } from "next/navigation";
+import { useAuth} from "@clerk/nextjs";
 import { useQuery } from "@tanstack/react-query";
 
 import { getSessionUser } from "@/actions/sidebar/get-session-user";
@@ -20,14 +20,13 @@ import { Shield } from "lucide-react";
 import ListView from "@/components/global/list-view";
 import { SessionUserCard } from "./session-user";
 import Show from "@/components/global/show";
+import ClientWrapper from "@/components/global/client-wrapper";
 
 
-export const ManageSession= () => {
+export const ManageSession = () => {
 
   const pathname = usePathname()
   const { userId } = useAuth()
-
-
 
   const sessionId = pathname?.split('/')[2] || ''
 
@@ -40,70 +39,70 @@ export const ManageSession= () => {
   } = useQuery<SafesessionUser[]>({
     queryKey: ["session-user", sessionId],
     queryFn: () => getSessionUser({ sessionId }),
+    enabled: !!sessionId
   });
 
 
   // TODO : Add the UI for the settings sidebar
   if (isPending) return <div className="size-4 bg-background shadow-neutral-50  rounded-md" />
 
-   if (isError) return <div>Error: {error.message}</div>
+  if (isError) return <div>Error: {error.message}</div>
 
-  if (!userId) {
-    return redirect('/')
-  }
 
   const isAdmin = users?.find(user => user.userId === userId && user.access === 'ADMIN')
 
 
   return (
-    <Show>
-      <Show.When
-        isTrue={!!isAdmin }
-      >
+    <ClientWrapper>
 
-        <Sheet
+      <Show>
+        <Show.When
+          isTrue={!!isAdmin}
         >
-          <SheetTrigger
-            asChild
-          >
-            <Button
-              variant="ghost"
-              className='size-8'
-              size={'icon'}
-            >
-              <Shield
-                className="size-4 animate-pulse"
-              />
-            </Button>
-          </SheetTrigger>
-          <SheetContent>
 
-            <SheetHeader>
-              <SheetTitle>
-                Manage Chat Sesstion
-              </SheetTitle>
-              <SheetDescription>
-                Manage the chat session and its participants
-              </SheetDescription>
-            </SheetHeader>
-
-            <div className="flex flex-col gap-4 mt-4">
-              <h3
-                className="text-sm font-semibold"
+          <Sheet >
+            <SheetTrigger asChild >
+              <Button
+                variant="ghost"
+                className='size-8'
+                size={'icon'}
               >
-                Participants
-              </h3>
-              <ListView
-                items={users}
-                render={(user: SafesessionUser) =>
-                  <SessionUserCard sessionUser={user} refetch={refetch} />}
-              />
-            </div>
-          </SheetContent>
+                <Shield
+                  className="size-4 animate-pulse"
+                />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
 
-        </Sheet >
-      </Show.When>
-    </Show>
+              <SheetHeader>
+                <SheetTitle>
+                  Manage Chat Sesstion
+                </SheetTitle>
+                <SheetDescription>
+                  Manage the chat session and its participants
+                </SheetDescription>
+              </SheetHeader>
+
+              <div className="flex flex-col gap-4 mt-4">
+                <h3
+                  className="text-sm font-semibold"
+                >
+                  Participants
+                </h3>
+                <ListView
+                  items={users}
+                  render={(user: SafesessionUser) =>
+                    <SessionUserCard sessionUser={user}
+                      refetch={refetch}
+                    />}
+                />
+              </div>
+            </SheetContent>
+
+          </Sheet >
+        </Show.When>
+      </Show>
+    </ClientWrapper>
   )
 }
 
